2c02c3a818c599f78c422bb09f52af201f6a56c4
obiwac <obiwac@gmail.com>
2023-05-28 17:00:01 +0200

    ~: Switch over to Rocket.rs + Maud

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+#![allow(macro_expanded_macro_exports_accessed_by_absolute_paths)]
+#![feature(decl_macro)]
+
+#[macro_use] extern crate rocket;
+extern crate maud;
+
+use maud::{Markup, html, DOCTYPE, PreEscaped};
+use rocket::fs::FileServer;
+
+macro_rules! relative {
+	($path: expr) => (concat!(env!("CARGO_MANIFEST_DIR"), $path))
+}
+
+macro_rules! include_static {
+	($path: expr) => (PreEscaped(include_str!(relative!(concat!("/public", $path)))))
+}
+
+fn base(content: Markup) -> Markup {
+	html! {
+		(DOCTYPE)
+
+		html lang="en" {
+			head {
+				meta charset="UTF-8"; // must be in the first 1024 bytes of the document
+				meta name="description" content="Info on ICPC-related stuff at UCLouvain"; // can't be longer than 275 characters as per Google's 2017 limit on the SERP
+				meta name="viewport" content="width=device-width,initial-scale=1";
+				meta name="robots" content="index,follow";
+				meta name="theme-color" content="#000000";
+
+				link rel="icon" type="image/png" href="https://cdn.shopify.com/s/files/1/1061/1924/products/Nerd_with_Glasses_Emoji_2a8485bc-f136-4156-9af6-297d8522d8d1_large.png?v=1571606036";
+				link rel="manifest" href="manifest.json";
+
+				// Apple PWA stuff
+
+				meta name="apple-mobile-web-app-capable" content="yes";
+				meta name="apple-mobile-web-app-status-bar-style" content="black-translucent";
+				meta name="apple-mobile-web-app-title" content="UCL ICPC";
+
+				// TODO keywords, google-site-verification, apple-touch-startup-image
+
+				title { "UCLouvain ICPC" }
+
+				// link rel="stylesheet" type="text/css" href="/public/main.css";
+
+				style {
+					(include_static!("/main.css"))
+				}
+			}
+
+			body {
+				(content)
+			}
+		}
+	}
+}
+
+#[get("/")]
+fn index() -> Markup {
+	base(html! {
+		h1 {
+			"UCLouvain "
+			b { "ICPC" }
+		}
+	})
+}
+
+// server stuff
+
+#[launch]
+fn rocket() -> _ {
+	let rocket = rocket::build();
+
+	rocket
+		.mount("/", routes![index])
+		.mount("/public", FileServer::from(relative!("/public")))
+}
